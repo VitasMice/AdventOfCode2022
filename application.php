e3a20a35cf0d419d99ee54944e42bd4d478db678
VitasMice <vitas.m1998@gmail.com>
2022-12-14 19:17:05 +0200

    Add day command

---
+++ application.php
@@ -0,0 +1,13 @@
+#!/usr/bin/env php
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+use Symfony\Component\Console\Application;
+use VitasMice\AoC22\Day;
+
+$application = new Application();
+
+$application->add(new Day());
+
+$application->run();
